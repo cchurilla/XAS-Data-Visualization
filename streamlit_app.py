af87cbdb062cefb58cc0104e30c345949e05a253
cchurilla <156447513+cchurilla@users.noreply.github.com>
2024-07-09 15:04:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
+import st.plotly_chart as go
 from scipy.signal import savgol_filter, windows
 from scipy.fft import fft, fftfreq
 import os
